dc2a244c45611cac4d05648b5dd35fa29bcd69fe
rafael <rafael@rafaels-MBP.T-mobile.com>
2018-01-11 01:11:42 -1000

    completed exercise 7

--- exercises.js
+++ exercises.js
@@ -46,28 +46,50 @@ This function will iterate through the person parameter and console.log the foll
 "President person was a great leader."
 */
 
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+for (var i = 0; i < presidents.length; i++)
+	console.log("The value at ", i, " is", presidents[i]);
+	console.log(presidents.length);
+
+function leaders(person){
+	for (var i = 0; i < presidents.length; i++)
+	console.log("President ", person[i], " was a great leader");
+}
+leaders(presidents);
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
-
-
+var stringOfNumbers = "";
+for (var i = 10; i <= 20; i++){
+	stringOfNumbers += i;
+}
+console.log(stringOfNumbers);
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
 
 Write a for-loop that will push even numbers to the `evenNumArr` array. We want to push 50 even even numbers starting from 0.
 Console.log your results.
 */
-
-
+var eveNumArr = [];
+for (var i = 0; i <= 50; i++){
+	eveNumArr.push(i*2);
+}
+console.log(eveNumArr);	
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
 
 Write a for-loop that will sum up odd numbers to the `oddSum` variable. We want to add 50 odd numbers starting from 1.
 Console.log your results.
 */
+  var oddSum = 0;
+  for (var i = 0; i < 50; i++){
+  	oddSum += (i * 2 + 1);
   
+  	
+  }
+console.log(oddSum);
 
 /* 7) Oops There It is
 Declare a variable named `oopsArray` and assign its to the following array: `[ 'turn' , , 'down' , , 'for' , , 'what' ]`.
@@ -75,6 +97,16 @@ Note that every odd index value in `oopsArray` is currently `undefined`. Using a
 
 [ 'turn' , 'nope' , 'down' , 'nope' , 'for' , 'nope' , 'what' ]
 */
+var oopsArray = [ "turn", , "down", , "for", , "what" ];
+
+for (var i = 0; i < oopsArray.length; i++){
+
+	if (i % 2 === 1){
+		oopsArray[i] = "nope";
+	}
+}
+console.log(oopsArray);
+
 
 
 /* 8) Is It There Oops
